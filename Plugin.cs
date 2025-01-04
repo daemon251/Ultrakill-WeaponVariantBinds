7cec1ee45193611717a73135e334666f92abf4b8
daemon <51888735+daemon251@users.noreply.github.com>
2025-01-04 15:13:30 -0600

    0.1.3 -> 0.1.4
    Now checking if some specific keys exist in prefMap instead of assuming they just exist, should prevent KeyNotFoundExceptions from occuring and bricking the mod for some users.

--- Plugin.cs
+++ Plugin.cs
@@ -119,10 +119,10 @@ public class Plugin : BaseUnityPlugin
                     //now check for if another weapon shares this bind AND if we are already on one of them. If we are, then switch to the next one on it (first by j, then by i). Else, switch to the weapon we intended.
                     if(MonoSingleton<GunControl>.Instance.currentSlot == i + 1 && MonoSingleton<GunControl>.Instance.currentVariation == j)
                     {
-                        Debug.Log("try to switch to next wep with same keycode");
+                        Debug.Log("WeaponVariantBinds - try to switch to next wep with same keycode");
                         if(switchToNextWeaponWithSameKeyCode(weaponKeyCodes[i, j], i, j))
                         {
-                            Debug.Log("switched to next wep with same keycode");
+                            Debug.Log("WeaponVariantBinds - switched to next wep with same keycode");
                             goto WeaponSwitched;
                         } 
                     }
@@ -148,7 +148,7 @@ public class Plugin : BaseUnityPlugin
                     tempDisableAutoSwitch = true;
                     weapon = MonoSingleton<GunControl>.Instance.currentWeapon;
 
-                    Debug.Log("key pressed");
+                    Debug.Log("WeaponVariantBinds - key pressed");
 
                     goto WeaponSwitched;
                 }
@@ -182,10 +182,20 @@ public class Plugin : BaseUnityPlugin
             SwapVariationIgnoreMod == true)
         {tempDisableAutoSwitch = true; weapon = MonoSingleton<GunControl>.Instance.currentWeapon;}
 
+        //you have to do it like this, doing it the normal way just doesnt work for some reason... Causes KeyNotFoundException I think for some reason for some people unless we check if the key exists?
+        bool scrollEnabled = false;
+        bool scrollVariation = false;
+        bool scrollReversed = false;
+
+        Debug.Log("WeaponVariantBinds - attempting to load extra prefs");
+        if(MonoSingleton<PrefsManager>.Instance.prefMap.ContainsKey("scrollEnabled")) {scrollEnabled =   (bool)MonoSingleton<PrefsManager>.Instance.prefMap["scrollEnabled"];}
+        if(MonoSingleton<PrefsManager>.Instance.prefMap.ContainsKey("scrollVariations")) {scrollVariation = (bool)MonoSingleton<PrefsManager>.Instance.prefMap["scrollVariations"];}
+        if(MonoSingleton<PrefsManager>.Instance.prefMap.ContainsKey("scrollReversed")) {scrollReversed =  (bool)MonoSingleton<PrefsManager>.Instance.prefMap["scrollReversed"];}
+
         if(Math.Abs(Input.GetAxis("Mouse ScrollWheel")) >= 0.1 &&
-           (bool)MonoSingleton<PrefsManager>.Instance.prefMap["scrollEnabled"]    == true &&
-           (bool)MonoSingleton<PrefsManager>.Instance.prefMap["scrollVariations"] == true &&
-           ScrollVariationIgnoreMod == true)
+           scrollEnabled            == true &&
+           scrollVariation          == true &&
+           ScrollVariationIgnoreMod == true  )
         {tempDisableAutoSwitch = true; weapon = MonoSingleton<GunControl>.Instance.currentWeapon;}
 
         //first two conditions should prevent OutOfBounds exceptions in case of modded weapons
@@ -207,18 +217,16 @@ public class Plugin : BaseUnityPlugin
                 bool weaponChanged = false;
                 bool reversedDirection = false;
 
-                bool scrollEnabled =   (bool)MonoSingleton<PrefsManager>.Instance.prefMap["scrollEnabled"]; //you have to do it like this, doing it the normal way just doesnt work for some god damn reason
-                bool scrollVariation = (bool)MonoSingleton<PrefsManager>.Instance.prefMap["scrollVariations"];
                 if(scrollEnabled && scrollVariation)
                 {
                     float value = Input.GetAxis("Mouse ScrollWheel");
-                    if((bool)MonoSingleton<PrefsManager>.Instance.prefMap["scrollReversed"]) {reversedDirection = Math.Abs(value) >= 0.1f && value > 0; }
+                    if(scrollReversed) {reversedDirection = Math.Abs(value) >= 0.1f && value > 0; }
                     else{reversedDirection = Math.Abs(value) >= 0.1f && value < 0;}
                 }
                 if(MonoSingleton<InputManager>.Instance.InputSource.PreviousVariation.WasCanceledThisFrame == true) {reversedDirection = true;}
 
-                Debug.Log("current: " + MonoSingleton<GunControl>.Instance.currentSlot + " " + MonoSingleton<GunControl>.Instance.currentVariation);
-                Debug.Log("weapon about to be forced");
+                Debug.Log("WeaponVariantBinds - current: " + MonoSingleton<GunControl>.Instance.currentSlot + " " + MonoSingleton<GunControl>.Instance.currentVariation);
+                Debug.Log("WeaponVariantBinds - weapon about to be forced");
                 if(reversedDirection == false)
                 {
                     if(slotList.Count > 1 && MonoSingleton<GunControl>.Instance.currentVariation == 0 && ignoreWeaponInCycle[slot - 1, MonoSingleton<GunControl>.Instance.currentVariation] == true) {MonoSingleton<GunControl>.Instance.ForceWeapon(slotList[1], true); weaponChanged = true;}
@@ -234,7 +242,7 @@ public class Plugin : BaseUnityPlugin
                     if(MonoSingleton<GunControl>.Instance.currentVariation == 0 && ignoreWeaponInCycle[slot - 1, MonoSingleton<GunControl>.Instance.currentVariation] == true) {MonoSingleton<GunControl>.Instance.ForceWeapon(slotList[slotList.Count - 1], true); weaponChanged = true;}
                     if(weaponChanged) {setLastUsedWeapon();}
                 }
-                Debug.Log("after: " + MonoSingleton<GunControl>.Instance.currentSlot + " " + MonoSingleton<GunControl>.Instance.currentVariation);
+                Debug.Log("WeaponVariantBinds - after: " + MonoSingleton<GunControl>.Instance.currentSlot + " " + MonoSingleton<GunControl>.Instance.currentVariation);
             }
         }
 
